2408e9b8180754afc545b8a817eb41e1d21fd278
brian-sigurdson <sigurdson.brian@gmail.com>
2022-11-30 10:23:56 -0500

    update

--- 2022-11-03-a/terraform/main.tf
+++ 2022-11-03-a/terraform/main.tf
@@ -116,8 +116,8 @@ resource "aws_lb_target_group" "asg" {
   vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    # path                = "/ping"
-    path                = "/"
+    path                = "/ping"
+    # path                = "/"
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 15
